c25710f16cc7ccb9ec4e38e42f094460479d81ba
Andrew Meleshnikov <andreymeleshnikov@gmail.com>
2022-12-10 16:52:02 +0300

    Initial commit

---
+++ src/test/java/pro/sky/course2/examinerservice/ExaminerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.course2.examinerservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExaminerServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
